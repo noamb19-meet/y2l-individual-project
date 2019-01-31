ac04c98f2752820e40395a1ccd35bc4dbf757963
noamb19-meet <noamb19@meet.mit.edu>
2019-01-31 15:34:07 +0200

    fixed threading error

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from model import *
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('sqlite:///cats.db')
+engine = create_engine('sqlite:///cats.db?check_same_thread=False')
 Base.metadata.create_all(engine)
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
